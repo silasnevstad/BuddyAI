711910750ea4b0672ef225c6c4528ffb97963fa8
Silas Nevstad <nevstads@gmail.com>
2023-05-17 19:37:03 -0400

    Title Logo Added

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,7 @@
 import './styles/Title.css';
 import { Link } from 'react-router-dom';
 import './styles/Buttons.css'
+import BuddyLogo from './images/buddyLogo.png';
 
 const Title = () => {
     const title = 'Buddy';
@@ -8,6 +9,7 @@ const Title = () => {
     return (
         <div className="title-container">
             <div className="title">
+                {/* <img src={BuddyLogo} alt="Buddy Logo" className="buddy-logo" /> */}
                 <Link to="/" className="transparent-button-large"> {title.toLocaleUpperCase()} </Link>
             </div>
         </div>
